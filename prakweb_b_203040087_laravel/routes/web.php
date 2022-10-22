2967ede30e124c4d3a59bdae3f811ccabf4a3976
HamzahHadiPermanaa <203040087@mail.unpas.ac.id>
2022-10-22 13:11:25 +0700

    Menambah Struktur Folder,Routes dan View

--- prakweb_b_203040087_laravel/routes/web.php
+++ prakweb_b_203040087_laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Hamzah Hadi Permana",
+        "email" => "203040087@mail.unpas.ac.id",
+        "image" => "hamzah.jpg"
+    ]);   
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
