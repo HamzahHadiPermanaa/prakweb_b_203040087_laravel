476a934b50e793c16147e70a9f0e41acc6f4b5c2
HamzahHadiPermanaa <203040087@mail.unpas.ac.id>
2022-10-22 18:29:38 +0700

    Membuat blade templating enggine

--- prakweb_b_203040087_laravel/routes/web.php
+++ prakweb_b_203040087_laravel/routes/web.php
@@ -14,17 +14,72 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Hamzah Hadi Permana",
         "email" => "203040087@mail.unpas.ac.id",
         "image" => "hamzah.jpg"
     ]);   
 });
 
+
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Hamzah Hadi Permana",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta soluta totam, doloribus, beatae dolor quaerat, tenetur itaque dolorem voluptates id fugit adipisci in! Tenetur magnam omnis libero error delectus, voluptatibus cumque reprehenderit aut reiciendis alias rerum quasi officia totam cum sed sit quo maiores adipisci culpa corrupti blanditiis ut veritatis nulla! Dicta non cupiditate maxime provident omnis. Corrupti iure, magni possimus voluptates repellendus hic placeat odio perspiciatis adipisci mollitia est accusantium a nihil dolores magnam assumenda porro? Quos, dolor inventore?"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "MR Crab",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quod sunt officiis nesciunt. Repudiandae officia inventore odit sed eveniet! Corrupti officia ad velit dolor nesciunt mollitia reiciendis harum vero cupiditate!"
+        ],
+    
+    ];
+    
+    return view('posts',[
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+
+// halaman single post
+Route::get('posts/{slug}', function($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Hamzah Hadi Permana",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta soluta totam, doloribus, beatae dolor quaerat, tenetur itaque dolorem voluptates id fugit adipisci in! Tenetur magnam omnis libero error delectus, voluptatibus cumque reprehenderit aut reiciendis alias rerum quasi officia totam cum sed sit quo maiores adipisci culpa corrupti blanditiis ut veritatis nulla! Dicta non cupiditate maxime provident omnis. Corrupti iure, magni possimus voluptates repellendus hic placeat odio perspiciatis adipisci mollitia est accusantium a nihil dolores magnam assumenda porro? Quos, dolor inventore?"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "MR Crab",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quod sunt officiis nesciunt. Repudiandae officia inventore odit sed eveniet! Corrupti officia ad velit dolor nesciunt mollitia reiciendis harum vero cupiditate!"
+        ],
+    
+    ];
+
+    $new_post =[];
+    foreach($blog_posts as $post) {
+        if($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+     return view('post',[
+        "title" => "Single Post",
+        "post" => $new_post
+     ]);
 });
