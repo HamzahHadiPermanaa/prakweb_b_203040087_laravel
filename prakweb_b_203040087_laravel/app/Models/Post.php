f793fc10a39fc6c212f807d2c3653098768d3fb8
HamzahHadiPermanaa <203040087@mail.unpas.ac.id>
2022-10-27 00:53:02 +0700

    Membuat model dan controller

---
+++ prakweb_b_203040087_laravel/app/Models/Post.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+
+class Post 
+{
+    private static $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Hamzah Hadi Permana",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta soluta totam, doloribus, beatae dolor quaerat, tenetur itaque dolorem voluptates id fugit adipisci in! Tenetur magnam omnis libero error delectus, voluptatibus cumque reprehenderit aut reiciendis alias rerum quasi officia totam cum sed sit quo maiores adipisci culpa corrupti blanditiis ut veritatis nulla! Dicta non cupiditate maxime provident omnis. Corrupti iure, magni possimus voluptates repellendus hic placeat odio perspiciatis adipisci mollitia est accusantium a nihil dolores magnam assumenda porro? Quos, dolor inventore?"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "MR Crab",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quod sunt officiis nesciunt. Repudiandae officia inventore odit sed eveniet! Corrupti officia ad velit dolor nesciunt mollitia reiciendis harum vero cupiditate!"
+        ]
+    
+    ];
+
+    public static function find($slug)
+    {
+        return self::$blog_posts;
+    }
+
+    public static function find($slug)
+    {
+        $posts = static::all();
+        return $post->firstWhere('slug', $slug);
+    }
+}
--- prakweb_b_203040087_laravel/routes/web.php
+++ prakweb_b_203040087_laravel/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,58 +30,6 @@ Route::get('/about', function () {
     ]);   
 });
 
-
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Hamzah Hadi Permana",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta soluta totam, doloribus, beatae dolor quaerat, tenetur itaque dolorem voluptates id fugit adipisci in! Tenetur magnam omnis libero error delectus, voluptatibus cumque reprehenderit aut reiciendis alias rerum quasi officia totam cum sed sit quo maiores adipisci culpa corrupti blanditiis ut veritatis nulla! Dicta non cupiditate maxime provident omnis. Corrupti iure, magni possimus voluptates repellendus hic placeat odio perspiciatis adipisci mollitia est accusantium a nihil dolores magnam assumenda porro? Quos, dolor inventore?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "MR Crab",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quod sunt officiis nesciunt. Repudiandae officia inventore odit sed eveniet! Corrupti officia ad velit dolor nesciunt mollitia reiciendis harum vero cupiditate!"
-        ],
-    
-    ];
-    
-    return view('posts',[
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
-
+Route::get('/posts', [PostController::class, 'index']);
 // halaman single post
-Route::get('posts/{slug}', function($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Hamzah Hadi Permana",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta soluta totam, doloribus, beatae dolor quaerat, tenetur itaque dolorem voluptates id fugit adipisci in! Tenetur magnam omnis libero error delectus, voluptatibus cumque reprehenderit aut reiciendis alias rerum quasi officia totam cum sed sit quo maiores adipisci culpa corrupti blanditiis ut veritatis nulla! Dicta non cupiditate maxime provident omnis. Corrupti iure, magni possimus voluptates repellendus hic placeat odio perspiciatis adipisci mollitia est accusantium a nihil dolores magnam assumenda porro? Quos, dolor inventore?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "MR Crab",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quod sunt officiis nesciunt. Repudiandae officia inventore odit sed eveniet! Corrupti officia ad velit dolor nesciunt mollitia reiciendis harum vero cupiditate!"
-        ],
-    
-    ];
-
-    $new_post =[];
-    foreach($blog_posts as $post) {
-        if($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-     return view('post',[
-        "title" => "Single Post",
-        "post" => $new_post
-     ]);
-});
+Route::get('posts/{slug}', [PostController::class, 'show']);
