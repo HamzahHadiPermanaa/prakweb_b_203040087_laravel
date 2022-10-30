9adea80477f46911392f0b4616c8781fdd09ad80
HamzahHadiPermanaa <203040087@mail.unpas.ac.id>
2022-10-30 10:38:28 +0700

    Membuat video 9 Database Seeder, seperti biasa dengan sabar dan teliti

--- prakweb_b_203040087_laravel/database/seeders/DatabaseSeeder.php
+++ prakweb_b_203040087_laravel/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,63 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Hamzah Hadi Permana',
+            'email' => '203040087@mail.unpas.ac.id',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Farhan Alfauzi',
+            'email' => '203040085@mail.unpas.ac.id',
+            'password' => bcrypt('54321')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programing',
+            'slug' => 'web-programing'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quidem.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quidem.Ipsum consectetur sapiente maxime necessitatibus eligendi perspiciatis amet tempore blanditiis at, sunt deserunt qui molestias repellendus ab nemo. Autem porro reiciendis ab tenetur! Facilis odit similique velit! Consequuntur voluptates sequi, laborum quas fugit corporis labore eos explicabo delectus eius eveniet ipsa veritatis possimus quaerat.Vel, veritatis odit suscipit, nobis laboriosam illo ab fugit temporibus dicta obcaecati minima ipsa deserunt? Eum, officia animi. Aspernatur et recusandae ex odio placeat officiis non hic aliquid impedit repudiandae iste expedita incidunt blanditiis soluta, eligendi facere veritatis necessitatibus, dolorem vel animi consequatur nisi iusto. Repellat?',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Dua ',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quidem.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quidem.Ipsum consectetur sapiente maxime necessitatibus eligendi perspiciatis amet tempore blanditiis at, sunt deserunt qui molestias repellendus ab nemo. Autem porro reiciendis ab tenetur! Facilis odit similique velit! Consequuntur voluptates sequi, laborum quas fugit corporis labore eos explicabo delectus eius eveniet ipsa veritatis possimus quaerat.Vel, veritatis odit suscipit, nobis laboriosam illo ab fugit temporibus dicta obcaecati minima ipsa deserunt? Eum, officia animi. Aspernatur et recusandae ex odio placeat officiis non hic aliquid impedit repudiandae iste expedita incidunt blanditiis soluta, eligendi facere veritatis necessitatibus, dolorem vel animi consequatur nisi iusto. Repellat?',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Tiga ',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quidem.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quidem.Ipsum consectetur sapiente maxime necessitatibus eligendi perspiciatis amet tempore blanditiis at, sunt deserunt qui molestias repellendus ab nemo. Autem porro reiciendis ab tenetur! Facilis odit similique velit! Consequuntur voluptates sequi, laborum quas fugit corporis labore eos explicabo delectus eius eveniet ipsa veritatis possimus quaerat.Vel, veritatis odit suscipit, nobis laboriosam illo ab fugit temporibus dicta obcaecati minima ipsa deserunt? Eum, officia animi. Aspernatur et recusandae ex odio placeat officiis non hic aliquid impedit repudiandae iste expedita incidunt blanditiis soluta, eligendi facere veritatis necessitatibus, dolorem vel animi consequatur nisi iusto. Repellat?',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Empat ',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quidem.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quidem.Ipsum consectetur sapiente maxime necessitatibus eligendi perspiciatis amet tempore blanditiis at, sunt deserunt qui molestias repellendus ab nemo. Autem porro reiciendis ab tenetur! Facilis odit similique velit! Consequuntur voluptates sequi, laborum quas fugit corporis labore eos explicabo delectus eius eveniet ipsa veritatis possimus quaerat.Vel, veritatis odit suscipit, nobis laboriosam illo ab fugit temporibus dicta obcaecati minima ipsa deserunt? Eum, officia animi. Aspernatur et recusandae ex odio placeat officiis non hic aliquid impedit repudiandae iste expedita incidunt blanditiis soluta, eligendi facere veritatis necessitatibus, dolorem vel animi consequatur nisi iusto. Repellat?',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
--- prakweb_b_203040087_laravel/resources/views/categories.blade.php
+++ prakweb_b_203040087_laravel/resources/views/categories.blade.php
@@ -11,4 +11,4 @@
    
     @endforeach
 
-@endsection
+@endsectionp
