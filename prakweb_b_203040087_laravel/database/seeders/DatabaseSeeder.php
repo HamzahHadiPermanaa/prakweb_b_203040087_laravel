bcde2e840c4cdcdd1b97684faf286c68124aeda2
HamzahHadiPermanaa <203040087@mail.unpas.ac.id>
2022-10-30 12:05:23 +0700

    Membuat video 10 Factory&Faker

--- prakweb_b_203040087_laravel/database/seeders/DatabaseSeeder.php
+++ prakweb_b_203040087_laravel/database/seeders/DatabaseSeeder.php
@@ -16,19 +16,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        
 
-        User::create([
-            'name' => 'Hamzah Hadi Permana',
-            'email' => '203040087@mail.unpas.ac.id',
-            'password' => bcrypt('12345')
-        ]);
+        // User::create([
+        //     'name' => 'Hamzah Hadi Permana',
+        //     'email' => '203040087@mail.unpas.ac.id',
+        //     'password' => bcrypt('12345')
+        // ]);
 
-        User::create([
-            'name' => 'Farhan Alfauzi',
-            'email' => '203040085@mail.unpas.ac.id',
-            'password' => bcrypt('54321')
-        ]);
+        // User::create([
+        //     'name' => 'Farhan Alfauzi',
+        //     'email' => '203040085@mail.unpas.ac.id',
+        //     'password' => bcrypt('54321')
+        // ]);
+
+        User::factory(3)->create();
 
         Category::create([
             'name' => 'Web Programing',
@@ -40,40 +42,42 @@ class DatabaseSeeder extends Seeder
             'slug' => 'personal'
         ]);
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quidem.',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quidem.Ipsum consectetur sapiente maxime necessitatibus eligendi perspiciatis amet tempore blanditiis at, sunt deserunt qui molestias repellendus ab nemo. Autem porro reiciendis ab tenetur! Facilis odit similique velit! Consequuntur voluptates sequi, laborum quas fugit corporis labore eos explicabo delectus eius eveniet ipsa veritatis possimus quaerat.Vel, veritatis odit suscipit, nobis laboriosam illo ab fugit temporibus dicta obcaecati minima ipsa deserunt? Eum, officia animi. Aspernatur et recusandae ex odio placeat officiis non hic aliquid impedit repudiandae iste expedita incidunt blanditiis soluta, eligendi facere veritatis necessitatibus, dolorem vel animi consequatur nisi iusto. Repellat?',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        Post::factory(20)->create();
 
-        Post::create([
-            'title' => 'Judul Ke Dua ',
-            'slug' => 'judul-ke-dua',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quidem.',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quidem.Ipsum consectetur sapiente maxime necessitatibus eligendi perspiciatis amet tempore blanditiis at, sunt deserunt qui molestias repellendus ab nemo. Autem porro reiciendis ab tenetur! Facilis odit similique velit! Consequuntur voluptates sequi, laborum quas fugit corporis labore eos explicabo delectus eius eveniet ipsa veritatis possimus quaerat.Vel, veritatis odit suscipit, nobis laboriosam illo ab fugit temporibus dicta obcaecati minima ipsa deserunt? Eum, officia animi. Aspernatur et recusandae ex odio placeat officiis non hic aliquid impedit repudiandae iste expedita incidunt blanditiis soluta, eligendi facere veritatis necessitatibus, dolorem vel animi consequatur nisi iusto. Repellat?',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quidem.',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quidem.Ipsum consectetur sapiente maxime necessitatibus eligendi perspiciatis amet tempore blanditiis at, sunt deserunt qui molestias repellendus ab nemo. Autem porro reiciendis ab tenetur! Facilis odit similique velit! Consequuntur voluptates sequi, laborum quas fugit corporis labore eos explicabo delectus eius eveniet ipsa veritatis possimus quaerat.Vel, veritatis odit suscipit, nobis laboriosam illo ab fugit temporibus dicta obcaecati minima ipsa deserunt? Eum, officia animi. Aspernatur et recusandae ex odio placeat officiis non hic aliquid impedit repudiandae iste expedita incidunt blanditiis soluta, eligendi facere veritatis necessitatibus, dolorem vel animi consequatur nisi iusto. Repellat?',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ke Tiga ',
-            'slug' => 'judul-ke-tiga',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quidem.',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quidem.Ipsum consectetur sapiente maxime necessitatibus eligendi perspiciatis amet tempore blanditiis at, sunt deserunt qui molestias repellendus ab nemo. Autem porro reiciendis ab tenetur! Facilis odit similique velit! Consequuntur voluptates sequi, laborum quas fugit corporis labore eos explicabo delectus eius eveniet ipsa veritatis possimus quaerat.Vel, veritatis odit suscipit, nobis laboriosam illo ab fugit temporibus dicta obcaecati minima ipsa deserunt? Eum, officia animi. Aspernatur et recusandae ex odio placeat officiis non hic aliquid impedit repudiandae iste expedita incidunt blanditiis soluta, eligendi facere veritatis necessitatibus, dolorem vel animi consequatur nisi iusto. Repellat?',
-            'category_id' => 2,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ke Dua ',
+        //     'slug' => 'judul-ke-dua',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quidem.',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quidem.Ipsum consectetur sapiente maxime necessitatibus eligendi perspiciatis amet tempore blanditiis at, sunt deserunt qui molestias repellendus ab nemo. Autem porro reiciendis ab tenetur! Facilis odit similique velit! Consequuntur voluptates sequi, laborum quas fugit corporis labore eos explicabo delectus eius eveniet ipsa veritatis possimus quaerat.Vel, veritatis odit suscipit, nobis laboriosam illo ab fugit temporibus dicta obcaecati minima ipsa deserunt? Eum, officia animi. Aspernatur et recusandae ex odio placeat officiis non hic aliquid impedit repudiandae iste expedita incidunt blanditiis soluta, eligendi facere veritatis necessitatibus, dolorem vel animi consequatur nisi iusto. Repellat?',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ke Empat ',
-            'slug' => 'judul-ke-empat',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quidem.',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quidem.Ipsum consectetur sapiente maxime necessitatibus eligendi perspiciatis amet tempore blanditiis at, sunt deserunt qui molestias repellendus ab nemo. Autem porro reiciendis ab tenetur! Facilis odit similique velit! Consequuntur voluptates sequi, laborum quas fugit corporis labore eos explicabo delectus eius eveniet ipsa veritatis possimus quaerat.Vel, veritatis odit suscipit, nobis laboriosam illo ab fugit temporibus dicta obcaecati minima ipsa deserunt? Eum, officia animi. Aspernatur et recusandae ex odio placeat officiis non hic aliquid impedit repudiandae iste expedita incidunt blanditiis soluta, eligendi facere veritatis necessitatibus, dolorem vel animi consequatur nisi iusto. Repellat?',
-            'category_id' => 2,
-            'user_id' => 2
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ke Tiga ',
+        //     'slug' => 'judul-ke-tiga',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quidem.',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quidem.Ipsum consectetur sapiente maxime necessitatibus eligendi perspiciatis amet tempore blanditiis at, sunt deserunt qui molestias repellendus ab nemo. Autem porro reiciendis ab tenetur! Facilis odit similique velit! Consequuntur voluptates sequi, laborum quas fugit corporis labore eos explicabo delectus eius eveniet ipsa veritatis possimus quaerat.Vel, veritatis odit suscipit, nobis laboriosam illo ab fugit temporibus dicta obcaecati minima ipsa deserunt? Eum, officia animi. Aspernatur et recusandae ex odio placeat officiis non hic aliquid impedit repudiandae iste expedita incidunt blanditiis soluta, eligendi facere veritatis necessitatibus, dolorem vel animi consequatur nisi iusto. Repellat?',
+        //     'category_id' => 2,
+        //     'user_id' => 1
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Ke Empat ',
+        //     'slug' => 'judul-ke-empat',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quidem.',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, quidem.Ipsum consectetur sapiente maxime necessitatibus eligendi perspiciatis amet tempore blanditiis at, sunt deserunt qui molestias repellendus ab nemo. Autem porro reiciendis ab tenetur! Facilis odit similique velit! Consequuntur voluptates sequi, laborum quas fugit corporis labore eos explicabo delectus eius eveniet ipsa veritatis possimus quaerat.Vel, veritatis odit suscipit, nobis laboriosam illo ab fugit temporibus dicta obcaecati minima ipsa deserunt? Eum, officia animi. Aspernatur et recusandae ex odio placeat officiis non hic aliquid impedit repudiandae iste expedita incidunt blanditiis soluta, eligendi facere veritatis necessitatibus, dolorem vel animi consequatur nisi iusto. Repellat?',
+        //     'category_id' => 2,
+        //     'user_id' => 2
+        // ]);
     }
 }
