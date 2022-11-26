163233149a954cc21059d337815eb8d3edaecbac
HamzahHadiPermanaa <203040087@mail.unpas.ac.id>
2022-11-27 00:30:45 +0700

    Membuat video 22 Preview,Update & Delete Image

--- prakweb_b_203040087_laravel/resources/views/dashboard/posts/create.blade.php
+++ prakweb_b_203040087_laravel/resources/views/dashboard/posts/create.blade.php
@@ -2,11 +2,11 @@
 
 @section('container')
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h2 class="h2">Create New Post</h2>
+    <h2 class="h2">Create New Post</h2>
 </div>
 
 <div class="col-lg-8">
-<form method="post" action="/dashboard/posts" class="mb-5" enctype="multipart/form-data">
+  <form method="post" action="/dashboard/posts" class="mb-5" enctype="multipart/form-data">
     @csrf
   <div class="mb-3">
     <label for="title" class="form-label">Title</label>
@@ -40,18 +40,19 @@
   </div>
   <div class="mb-3">
     <label for="image" class="form-label">Post Image</label>
-    <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image">
+    <img class="img-preview img-fluid mb-3 col-sm-5">
+    <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" onchange="previewImage()">
     @error('image')
-    <div class="invalid-feedback">
-      {{ $message }}
-    </div>
+      <div class="invalid-feedback">
+        {{ $message }}
+      </div>
     @enderror
   </div>
   <div class="mb-3">
     <label for="body" class="form-label">Body</label>
-    @error('body')
-      <p class="text-danger">{{ $message }}</p>
-    @enderror
+      @error('body')
+        <p class="text-danger">{{ $message }}</p>
+      @enderror
     <input id="body" type="hidden" name="body" value="{{ old('body') }}">
     <trix-editor input="body"></trix-editor>
   </div>
@@ -73,6 +74,21 @@
   document.addEventListener('trix-file-accept', function(e){
     e.preventDefault();
   })
+
+  function previewImage(){
+    const image = document.querySelector('#image');
+    const imgPreview = document.querySelector('.img-preview');
+
+    imgPreview.style.display = 'block';
+
+    const oFReader = new FileReader();
+    oFReader.readAsDataURL(image.files[0]);
+
+    oFReader.onload = function(oFREvent) {
+      imgPreview.src = oFREvent.target.result;
+    }
+  }
+
 
 </script>
 @endsection
